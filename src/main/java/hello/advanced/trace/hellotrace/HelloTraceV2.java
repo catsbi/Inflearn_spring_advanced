6c15356fe78f379714c410f3f2b81829235f74bf
catsbi <killfine198@gmail.com>
2021-11-19 11:06:24 +0900

    Initial commit

---
+++ src/main/java/hello/advanced/trace/hellotrace/HelloTraceV2.java
@@ -0,0 +1,70 @@
+package hello.advanced.trace.hellotrace;
+
+import hello.advanced.trace.TraceId;
+import hello.advanced.trace.TraceStatus;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class HelloTraceV2 {
+    private static final String START_PREFIX = "-->";
+    private static final String COMPLETE_PREFIX = "<--";
+    private static final String EX_PREFIX = "<X-";
+
+
+    public TraceStatus begin(String message) {
+        final TraceId traceId = new TraceId();
+        final long startTimeMs = System.currentTimeMillis();
+        log.info("[{}] {}{}", traceId.getId(), addSpace(START_PREFIX, traceId.getLevel()), message);
+        return new TraceStatus(traceId, startTimeMs, message);
+    }
+
+    //V2 에서 추가된 메서드
+    public TraceStatus beginSync(TraceId beforeTraceId, String message) {
+        final TraceId nextId = beforeTraceId.createNextId();
+        final long startTimeMs = System.currentTimeMillis();
+        log.info("[{}] {}{}", nextId.getId(), addSpace(START_PREFIX, nextId.getLevel()), message);
+        return new TraceStatus(nextId, startTimeMs, message);
+    }
+
+    public void end(TraceStatus status) {
+        complete(status, null);
+    }
+
+    public void exception(TraceStatus status, Exception e) {
+        complete(status, e);
+    }
+
+    private void complete(TraceStatus status, Exception e) {
+        final long stopTimeMs = System.currentTimeMillis();
+        final long resultTimeMs = stopTimeMs - status.getStartTimeMs();
+        final TraceId traceId = status.getTraceId();
+
+        if (e == null) {
+            log.info("[{}] {}{} time={}ms", traceId.getId(),
+                    addSpace(COMPLETE_PREFIX, traceId.getLevel()), status.getMessage(),
+                    resultTimeMs);
+        } else {
+            log.info("[{}] {}{} time={}ms ex={}", traceId.getId(),
+                    addSpace(EX_PREFIX, traceId.getLevel()), status.getMessage(), resultTimeMs,
+                    e.toString());
+        }
+    }
+
+    private static String addSpace(String prefix, int level) {
+        final StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < level; i++) {
+            stringBuilder.append(composePrefix(i, level - 1, prefix));
+        }
+        return stringBuilder.toString();
+    }
+
+    private static String composePrefix(int index, int level, String prefix) {
+        if (index == level) {
+            return "|" + prefix;
+        }
+        return "|   ";
+    }
+
+}
---
+++ src/test/java/hello/advanced/trace/hellotrace/HelloTraceV1Test.java
@@ -0,0 +1,26 @@
+package hello.advanced.trace.hellotrace;
+
+import hello.advanced.trace.TraceStatus;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HelloTraceV1Test {
+
+    @Test
+    void begin_end() {
+        final HelloTraceV1 trace = new HelloTraceV1();
+
+        final TraceStatus status = trace.begin("hello");
+        trace.end(status);
+    }
+
+    @Test
+    void begin_exception() {
+        final HelloTraceV1 trace = new HelloTraceV1();
+
+        final TraceStatus status = trace.begin("hello");
+        trace.exception(status, new IllegalStateException());
+    }
+
+}
